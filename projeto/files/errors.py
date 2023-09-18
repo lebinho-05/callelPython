02285e3556c11d3c93298b705356d2dc69d166a6
lebinho-05 <140989206+lebinho-05@users.noreply.github.com>
2023-09-18 14:43:44 -0300

    Delete projeto/files/errors.py

--- projeto/files/errors.py
+++
@@ -1,6 +0,0 @@
-class FailureFunctionOrClass(Exception):...
-
-def failure(f):
-    def wrapper(*args, **kwargs):
-        raise FailureFunctionOrClass(f"{f.__name__} is a flawed function or class")
-    return wrapper
